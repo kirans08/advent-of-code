163d1eb220a035186ad59411638a283dda3c7eb9
Kiran S <18kirans@gmail.com>
2021-01-28 00:13:11 +0530

    Advent of Code 2019 - Day 02 - Move Intcode Computer to new file

--- 2019/02/02A.js
+++ 2019/02/02A.js
@@ -1,25 +1,11 @@
 const fs = require('fs');
 const input = fs.readFileSync('input', 'utf8');
+const IntcodeComputer = require('./IntcodeComputer');
 
-const codes = input.split(',').map(Number);
-codes[1] = 12;
-codes[2] = 2;
+const program = input.split(',').map(Number);
+program[1] = 12;
+program[2] = 2;
 
-let pointer = 0;
+const computer = new IntcodeComputer(program);
 
-while (pointer < codes.length) {
-
-    const op = codes[pointer++];
-
-    if (op == 99) {
-        break;
-    }
-
-    const input1 = codes[codes[pointer++]];
-    const input2 = codes[codes[pointer++]];
-
-    codes[codes[pointer++]] = (op == 1) ? input1 + input2 : input1 * input2;
-
-}
-
-console.log(codes[0])
+console.log(computer.output());
--- 2019/02/02B.js
+++ 2019/02/02B.js
@@ -1,46 +1,22 @@
 const fs = require('fs');
 const input = fs.readFileSync('input', 'utf8');
+const IntcodeComputer = require('./IntcodeComputer');
 
-const codes = input.split(',').map(Number);
-codes[1] = 'x';
-codes[2] = 'y';
+const program = input.split(',').map(Number);
+const OUTPUT = 19690720;
 
-let pointer = 0;
+for (let i=0; i<=99; i++) {
+    for (let j=0; j<=99; j++) {
 
-while (pointer < codes.length) {
+        program[1] = i;
+        program[2] = j;
+        const computer = new IntcodeComputer(program);
 
-    const op = codes[pointer++];
+        if (computer.output() == OUTPUT) {
 
-    if (op == 99) {
-        break;
-    }
+            console.log(i.toString().padStart(2, '0') + j.toString().padStart(2, '0'));
 
-    const input1 = codes[codes[pointer++]];
-    const input2 = codes[codes[pointer++]];
-
-    let result;
-    if (isNaN(input1) || isNaN(input2)) {
-        result = `(${input1}${op == 1 ? '+' : '*'}${input2})`;
-    }
-    else {
-        result = (op == 1) ? input1 + input2 : input1 * input2;
-    }
-
-    codes[codes[pointer++]] = result;
-
-}
-
-const findXY = (equation) => {
-
-    for (let i=0;i<=99; i++) {
-        for (let j=0; j<=99; j++) {
-            let t = equation.replace(/x/g, i).replace(/y/g, j);
-            if (eval(t) == 19690720) {
-                return i.toString().padStart(2, '0') + j.toString().padStart(2, '0');
-            }
         }
+
     }
-
 }
-
-console.log(findXY(codes[0]));
