668ace3d5d6c5acce134e86890ddb78c7125a477
Kiran S <18kirans@gmail.com>
2021-02-05 01:50:03 +0530

    Advent of Code 2019 - Day 19

---
+++ 2019/19/19AB.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync('input', 'utf8');
+const IntcodeComputer = require('../shared/IntcodeComputerV3');
+
+const program = input.split(',').map(Number);
+const drone = new IntcodeComputer(program);
+
+const GRID_LENGTH = 50;
+let count = 0;
+
+const isDronePulled = (x, y) => {
+
+    drone.reset();
+    drone.input([x, y]);
+
+    return drone.output() === 1;
+
+}
+
+// Part 1
+for (let i = 0; i < GRID_LENGTH; i++) {
+
+    for (let j = 0; j < GRID_LENGTH; j++) {
+
+        isDronePulled(i, j) && count++;
+
+    }
+
+}
+
+console.log(count);
+
+// Part 2
+const SHIP_DIMENSION_OFFSET = 99;
+let leftX = -1, topY = -1, rightX, bottomY = topY + SHIP_DIMENSION_OFFSET;
+
+do {
+
+    bottomY++;
+    topY++;
+    while(isDronePulled(++leftX, bottomY) == 0);
+
+    rightX = leftX + SHIP_DIMENSION_OFFSET;
+
+} while(!isDronePulled(rightX, topY))
+
+console.log((leftX*10000) + topY);
